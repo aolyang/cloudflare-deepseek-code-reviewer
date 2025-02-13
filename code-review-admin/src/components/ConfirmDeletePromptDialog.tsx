d3942a38ace596fa1000565dccc47c5792c3bfe4
aolyang <aolyang@foxmail.com>
2025-02-14 00:04:01 +0800

    feat: display prompt info in delete dialog

--- code-review-admin/src/components/ConfirmDeletePromptDialog.tsx
+++ code-review-admin/src/components/ConfirmDeletePromptDialog.tsx
@@ -1,4 +1,4 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material"
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Typography } from "@mui/material"
 
 import { deletePrompt } from "@/src/actions/prompts"
 import type { Prompt } from "@/src/utils/prompts"
@@ -25,7 +25,14 @@ const ConfirmDeletePromptDialog = ({
         <Dialog open={open} onClose={onClose}>
             <DialogTitle>Confirm Delete</DialogTitle>
             <DialogContent>
-                Are you sure you want to delete the prompt &#34;{prompt?.name}&#34;?
+                <Typography>Are you sure you want to delete the prompt &#34;{prompt?.name}&#34;?</Typography>
+                <DialogContentText>
+                    <Typography>command: /{prompt?.name}</Typography>
+                    <Typography>model: {prompt?.model}</Typography>
+                    {prompt?.messages.map((message, index) => (
+                        <Typography key={index}>{message.role} {"{  " + message.content + "  }"}</Typography>
+                    ))}
+                </DialogContentText>
             </DialogContent>
             <DialogActions>
                 <Button onClick={onClose} color="inherit">Cancel</Button>
