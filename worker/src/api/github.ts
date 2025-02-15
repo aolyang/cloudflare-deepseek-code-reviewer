353b1395c18cd776dc56993aa80078a23cb3fa76
aolyang <aolyang@foxmail.com>
2025-02-15 16:17:17 +0800

    test: action can receive

--- worker/src/api/github.ts
+++ worker/src/api/github.ts
@@ -1,28 +1,44 @@
 import { Hono } from "hono"
+
 import { githubApp } from "../utils/github"
 
 const github = new Hono<{ Bindings: CloudflareEnv }>()
 
+let app: ReturnType<typeof githubApp>
+
 github.post("/webhook", async (c) => {
-    const body = await c.req.json()
+    if (!app) app = githubApp(c)
+
     const signature = c.req.header("x-hub-signature-256")
 
-    if (!signature) {
-        return c.json({ error: "Signature missing" }, 400)
-    }
-
-    const app = githubApp(c)
+    if (!signature) return c.json({ error: "Signature missing" }, 400)
 
     try {
-        await app.webhooks.verify(body, signature)
+        await app.webhooks.verify(await c.req.text(), signature)
     } catch (error) {
         return c.json({ error: "Invalid signature" }, 400)
     }
 
+    const body = await c.req.json()
+    console.log("body", body)
     if (body.action === "opened" && body.issue) {
         return c.json({ message: "Welcome! Issue created." })
     }
 
+    app.webhooks.on("issues.opened", async ({ octokit, payload }) => {
+        console.log("issues.opened", payload)
+        await octokit.request("POST /repos/{owner}/{repo}/issues/{issue_number}/comments",
+            {
+                owner: payload.repository.owner.login,
+                repo: payload.repository.name,
+                issue_number: payload.issue.number,
+                body: "Hello there from [octokit](https://github.com/octokit/app.js/)"
+            }
+        )
+    })
+    app.webhooks.on("commit_comment.created", async ({ octokit, payload }) => {
+        console.log("Commit comment created", payload)
+    })
     return c.json({ message: "Event not handled" })
 })
 
--- worker/src/middleware/auth.ts
+++ worker/src/middleware/auth.ts
@@ -5,6 +5,7 @@ const auth = new Hono<{ Bindings: CloudflareEnv }>()
 
 auth.use("/api/*", (c, next) => {
     if (c.req.path.startsWith("/api/public")) return next()
+    if (c.req.path.startsWith("/api/github")) return next()
     return bearerAuth({
         verifyToken: (token) => token === c.env.AUTH_TOKEN
     })(c, next)
