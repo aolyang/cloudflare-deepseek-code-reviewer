7b1bb6f8a8d53caad156f61cf074ab15a4601c36
aolyang <aolyang@foxmail.com>
2025-02-17 13:59:54 +0800

    feat: import prompts json

--- worker/src/api/prompts.ts
+++ worker/src/api/prompts.ts
@@ -1,7 +1,9 @@
 import { Hono } from "hono"
 import { describeRoute } from "hono-openapi"
+import { z } from "zod"
 
-import { ResponsePromptsSchema } from "./prompts.schema"
+import { promptOverLimit } from "../utils/prompt"
+import { PromptSchema, ResponsePromptsSchema } from "./prompts.schema"
 
 const prompts = new Hono<{ Bindings: CloudflareEnv }>()
 
@@ -28,4 +30,31 @@ prompts.get("/",
     }
 )
 
+prompts.put("/",
+    describeRoute({
+        description: "import prompts json"
+    }),
+    async (c) => {
+        const body = await c.req.text()
+        const parse = z.array(PromptSchema).safeParse(JSON.parse(body))
+        const failed: string[] = []
+
+        if (!parse.success) return c.json({ message: "JSON parse failed" })
+
+        for (const prompt of parse.data) {
+            const { str, oversize } = promptOverLimit(prompt)
+            if (oversize) {
+                failed.push(prompt.name)
+            } else {
+                await c.env.prompts.put(prompt.name, str)
+            }
+        }
+
+        return c.json({
+            message: failed.length
+                ? `${failed.join(",")} are parse failed`
+                : "parse data success"
+        })
+    }
+)
 export default prompts
